397d77ae7a9e8922468b905a6611f5ebff13cfbf
Joshua Seltzer <joshua.seltzer90@gmail.com>
2025-02-27 20:49:09 -0500

    Reimplement the redirect URL logic to use a GET request instead of a HEAD since this is a more efficient way to potentially download the podcast if hosting on the local server. Set a download chunk size when saving the podcast to the server

--- utilities.py
+++ utilities.py
@@ -18,7 +18,9 @@ from datetime import datetime
 
 log = logging.getLogger('overcast-sonos')
 
-INVALID_HOSTS = ['megaphone.fm', 'podtoo.com']
+
+INVALID_PODCAST_HOSTS = ['megaphone.fm', 'podtoo.com']
+DOWNLOAD_CHUNK_SIZE = 12 * 1024 * 1024   # 12 MB
 USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.6 Safari/605.1.15'
 
 
@@ -43,41 +45,48 @@ def duration_in_seconds(duration_str):
 # Works out the final URL for those podcast platforms that redirect to another URL
 # If the redirected URL has a #t= timecode in it, we remove this as the Sonos player can't play these back, and it fixes compatibility with requests 2.19 and higher
 def final_redirect_url(url, title, podcast_title, local_ip, local_port, local_download_dir):
-    redirected_url = requests.head(url, allow_redirects=True).url
-    if url != redirected_url:
-        log.debug(f"Redirected url {url} to {redirected_url}")
+    # in a previous version of this logic, a HEAD request was used but some servers did not redirect properly unless a GET was sent
+    with requests.get(url, allow_redirects=False, stream=True, headers={'User-Agent': USER_AGENT}) as response:
+        if response.is_redirect:
+            redirected_url = response.headers['Location']
+            log.info(f"Redirected {url} to {redirected_url}")
+            return final_redirect_url(redirected_url, title, podcast_title, local_ip, local_port, local_download_dir)
+        elif response.ok:
+            log.info(f"Final URL for this podcast: {url}")
 
-    # for certain podcasts, the '#=' is added to the audio URL which causes Sonos to fail to connect
-    regex='#t=[0-9]*$'
-    if re.search(regex, redirected_url):
-        log.debug("Truncating the #t= part of the audio URL")
-        redirected_url = re.sub(regex, '', redirected_url)
+            # for certain podcasts, the '#=' is added to the audio URL which causes Sonos to fail to connect
+            regex='#t=[0-9]*$'
+            if re.search(regex, url):
+                log.info("Truncating the #t= part of the URL")
+                url = re.sub(regex, '', url)
 
-    # Check the hostname for any invalid hosts (i.e. ones that do not play correctly on Sonos).
-    # If an invalid host is found and local server information is provided, download the file directly to the system
-    # and then return that URL for Sonos to stream from.
-    parsed_url = urllib.parse.urlparse(redirected_url)
-    if any(invalid_host in parsed_url.hostname for invalid_host in INVALID_HOSTS):
-        log.info(f"\"{podcast_title}\" is hosted from a URL which will refuse to play on Sonos ({parsed_url.hostname})")
+            # Check the hostname for any invalid hosts (i.e. ones that do not play correctly on Sonos).
+            # If an invalid host is found and local server information is provided, download the file directly to the system
+            # and then return that URL for Sonos to stream from.
+            parsed_url = urllib.parse.urlparse(url)
+            if any(invalid_host in parsed_url.hostname for invalid_host in INVALID_PODCAST_HOSTS):
+                log.info(f"\"{podcast_title}\" is hosted from a URL which will refuse to play on Sonos ({parsed_url.hostname})")
 
-        # if local parameters were given, attempt to download and host the podcast directly from this server
-        file_ext = os.path.splitext(parsed_url.path)[1]
-        if file_ext != '' and local_ip and local_port and local_download_dir:
-            # get a valid file path for the file as it will be downloaded locally to this server
-            podcast_dir = slugify(podcast_title)
-            filename = f'{slugify(title)}{file_ext}'
-            file_dir = os.path.join(local_download_dir, podcast_dir)
-            full_file_path = os.path.join(file_dir, filename)
-            if not os.path.exists(full_file_path):
-                # since the file does not exist locally, download it now
-                os.makedirs(file_dir, exist_ok=True)
-                log.info(f"Downloading podcast to {full_file_path} from {redirected_url}")
-                download_file(redirected_url, full_file_path)
+                # if local parameters were given, attempt to download and host the podcast directly from this server
+                file_ext = os.path.splitext(parsed_url.path)[1]
+                if file_ext != '' and local_ip and local_port and local_download_dir:
+                    # get a valid file path for the file as it will be downloaded locally to this server
+                    podcast_dir = slugify(podcast_title)
+                    filename = f'{slugify(title)}{file_ext}'
+                    file_dir = os.path.join(local_download_dir, podcast_dir)
+                    full_file_path = os.path.join(file_dir, filename)
+                    if not os.path.exists(full_file_path):
+                        # since the file does not exist locally, download it now
+                        os.makedirs(file_dir, exist_ok=True)
+                        log.info(f"Downloading podcast to {full_file_path} from {url}")
+                        with open(full_file_path, 'wb') as f:
+                            shutil.copyfileobj(response.raw, f, length=DOWNLOAD_CHUNK_SIZE)
 
-            # create the URL that will be used to host this podcast file
-            redirected_url = f"http://{local_ip}:{local_port}/{podcast_dir}/{filename}"
-
-    return redirected_url
+                    # create the URL that will be used to host this podcast file
+                    return f"http://{local_ip}:{local_port}/{podcast_dir}/{filename}"
+        else:
+            log.error(f"Error trying to determine the final URL for podcast: {response}")
+            return ""
 
 
 # Turns a string like 'Dec 2, 2020 • 171 min' or 'Jan 13 • 147 min' into a date
@@ -144,13 +153,3 @@ def slugify(value, allow_unicode=False):
         )
     value = re.sub(r"[^\w\s-]", "", value.lower())
     return re.sub(r"[-\s]+", "-", value).strip("-_")
-
-
-# Modified solution from https://stackoverflow.com/a/39217788/1102981
-def download_file(url, local_filename):
-    with requests.get(url, stream=True, headers={'User-Agent': USER_AGENT}) as r:
-        if r.ok:
-            with open(local_filename, 'wb') as f:
-                shutil.copyfileobj(r.raw, f)
-        else:
-            log.info(f'There was an error downloading the podcast locally: {r}')
