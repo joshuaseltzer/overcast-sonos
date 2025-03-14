69d02e80263aad430eab1190b1243991b41717ff
Joshua Seltzer <joshua.seltzer90@gmail.com>
2025-03-13 20:33:31 -0400

    Decrease the download chunk size to try to avoid Sonos from timing out when streaming files from the local server

--- utilities.py
+++ utilities.py
@@ -20,7 +20,7 @@ log = logging.getLogger('overcast-sonos')
 
 
 INVALID_PODCAST_HOSTS = ['megaphone.fm', 'podtoo.com']
-DOWNLOAD_CHUNK_SIZE = 12 * 1024 * 1024   # 12 MB
+DOWNLOAD_CHUNK_SIZE = 256 * 1024   # 256 KB
 USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.6 Safari/605.1.15'
 
 
